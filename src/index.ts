1ef114f54957fd8d2049443aa6727a9c129df03c
ShadeSwap <dev@shadeswap.io>
2026-01-23 16:45:00 +0300

    feat: add ZK privacy layer

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ export type { ShadeSwapConfig, SwapOptions, SwapResult, SwapRules, ApprovalReque
 // commit 0 - 2026-01-15T10:22:00
 // commit 1 - 2026-01-17T14:05:00
 // commit 2 - 2026-01-20T11:30:00
+// commit 3 - 2026-01-23T16:45:00
