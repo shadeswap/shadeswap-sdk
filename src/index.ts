1247b4dcc2129ea55448f1261b8747d31aebf642
ShadeSwap <dev@shadeswap.io>
2026-02-04 15:30:00 +0300

    feat: add spending rules engine

--- src/index.ts
+++ src/index.ts
@@ -9,3 +9,4 @@ export type { ShadeSwapConfig, SwapOptions, SwapResult, SwapRules, ApprovalReque
 // commit 5 - 2026-01-28T13:00:00
 // commit 6 - 2026-01-30T17:20:00
 // commit 7 - 2026-02-02T10:10:00
+// commit 8 - 2026-02-04T15:30:00
