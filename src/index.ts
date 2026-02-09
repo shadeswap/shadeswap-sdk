b5f984ec6d9e7c4582252c479947f5215e65c537
ShadeSwap <dev@shadeswap.io>
2026-02-09 14:20:00 +0300

    chore: add CI workflow

--- src/index.ts
+++ src/index.ts
@@ -11,3 +11,4 @@ export type { ShadeSwapConfig, SwapOptions, SwapResult, SwapRules, ApprovalReque
 // commit 7 - 2026-02-02T10:10:00
 // commit 8 - 2026-02-04T15:30:00
 // commit 9 - 2026-02-06T11:00:00
+// commit 10 - 2026-02-09T14:20:00
