7da1ebd5b1e2f28038be2124dad6a424853abfd0
ShadeSwap <dev@shadeswap.io>
2026-03-10 09:30:00 +0300

    feat: add agent ID support

--- src/index.ts
+++ src/index.ts
@@ -25,3 +25,4 @@ export type { ShadeSwapConfig, SwapOptions, SwapResult, SwapRules, ApprovalReque
 // commit 21 - 2026-03-03T14:30:00
 // commit 22 - 2026-03-05T10:15:00
 // commit 23 - 2026-03-07T15:00:00
+// commit 24 - 2026-03-10T09:30:00
