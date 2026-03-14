a2470b3ae3002d2658497a3660eb953e2a561ee5
ShadeSwap <dev@shadeswap.io>
2026-03-14 11:00:00 +0300

    docs: add quickstart guide

--- src/index.ts
+++ src/index.ts
@@ -27,3 +27,4 @@ export type { ShadeSwapConfig, SwapOptions, SwapResult, SwapRules, ApprovalReque
 // commit 23 - 2026-03-07T15:00:00
 // commit 24 - 2026-03-10T09:30:00
 // commit 25 - 2026-03-12T13:45:00
+// commit 26 - 2026-03-14T11:00:00
